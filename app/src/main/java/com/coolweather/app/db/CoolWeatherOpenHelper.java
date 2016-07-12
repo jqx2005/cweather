e34565b45516a73cee719418e837c4198ebcc2d6
jqx <jqx55ah@gmail.com>
2016-07-12 20:34:35 +0800

    数据库部分暂时完成到这里

--- app/src/main/java/com/coolweather/app/db/CoolWeatherOpenHelper.java
+++ app/src/main/java/com/coolweather/app/db/CoolWeatherOpenHelper.java
@@ -22,7 +22,8 @@ public class CoolWeatherOpenHelper extends SQLiteOpenHelper{
     public static final String CREATE_CITY ="create table City("
             +"id integer primary key autoincrement,"
             +"city_name text,"
-            +"city_code text)";
+            +"city_code text,"
+            +"province_id integer)";
     /**
      * County表 建表语句
      */
